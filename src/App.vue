f8835540611b9ba8655eafd7f0eab5b6f8ad2c47
MarcoBagnasco <bagna.hbk@gmail.com>
2021-05-04 22:43:41 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,47 @@
 <template>
   <div id="app">
-    
+    <!-- HEADER -->
+    <Header />
+    <!-- MAIN -->
+    <!-- FOOTER -->
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue';
 
 export default {
   name: 'App',
   components: {
+    Header,
   }
 }
 </script>
 
 <style lang="scss">
-
+// GENERAL
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'Fira Sans Condensed', sans-serif;
+  font-size: 14px;
+}
+// UTILITIES
+  .container{
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  // Flex
+  .flex{
+    display: flex;
+  }
+  .jc-between{
+    justify-content: space-between;
+  }
+  .ai-center{
+    align-items: center;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,123 @@
+<template>
+  <header>
+      <div class="container flex jc-between">
+          <img src="../assets/img/dc-logo.png" alt="" class="logo">
+          <nav>
+              <ul class="flex">
+                  <li v-for="(item, index) in navLinks" :key="index"   class="flex ai-center" :class="{active: item.current}"><a href="/">{{item.text}}</a></li>
+              </ul>
+          </nav>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        return {
+            navLinks:[
+                {
+                    text: 'characters',
+                    url: '/characters',
+                    current: false,
+                },
+                {
+                    text: 'comics',
+                    url: '/comics',
+                    current: false,
+                },
+                {
+                    text: 'movies',
+                    url: '/movies',
+                    current: false,
+                },
+                {
+                    text: 'tv',
+                    url: '/tv',
+                    current: false,
+                },
+                {
+                    text: 'games',
+                    url: '/games',
+                    current: false,
+                },
+                {
+                    text: 'collectibles',
+                    url: '/collectibles',
+                    current: false,
+                },
+                {
+                    text: 'videos',
+                    url: '/videos',
+                    current: false,
+                },
+                {
+                    text: 'fans',
+                    url: '/fans',
+                    current: false,
+                },
+                {
+                    text: 'news',
+                    url: '/news',
+                    current: false,
+                },
+                {
+                    text: 'shop',
+                    url: '/shop',
+                    current: false,
+                },
+            ],
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        height: 120px;
+        background-color: #eee;
+    }
+    .container{
+        height: 100%;
+    }
+    img{
+        align-self: center;
+        height: 80px;
+    }
+    ul{
+        height: 100%;
+        list-style: none;
+    }
+    li{
+        position: relative;
+        margin: 0 15px;
+        overflow-y: hidden; ;
+    }
+    li:after{
+        content: '';
+        position: absolute;
+        bottom: -6px;
+        left: 0;
+        width: 100%;
+        height: 6px;
+        background-color: #0282f9;
+        transition: bottom .4s .1s;
+    }
+    li:hover:after,
+    li.active:after{
+       bottom: 0;
+    }
+    li a{
+        /* height: 100%; */
+        text-decoration: none;
+        text-transform: uppercase;
+        font-weight: 500;
+        color: #000;
+        transition: color .2s;
+    }
+    li a:hover,
+    li.active a{
+        color: #0282f9;
+    }
+</style>
